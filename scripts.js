5a55bd55936f73078282420f2a9c4ef8ed80a1c4
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-10-27 08:17:47 +0200

    Error Handling

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  if (dividend === "" || divider === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
+  if (divider === "0") {
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+    console.error("Error: Division by zero is not allowed");
+    return;
+  }
+  if (isNaN(dividend)|| isNaN(divider)) {
+  //
+    document.body.innerHTML ="<p>Something critical went wrong. Please reload the page</p>";
+    console.error("Error: Invalid number provided");
+    return;
+  }
   result.innerText = Math.trunc(dividend / divider);
-});
+ });
